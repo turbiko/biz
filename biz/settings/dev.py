f4eab36ef40415c78e2a82c951496fb16d32a941
Andriy Voznyuk <andreyv@ukr.net>
2022-09-12 20:12:39 +0200

    Update dev.py

--- biz/settings/dev.py
+++ biz/settings/dev.py
@@ -15,24 +15,17 @@ CSRF_TRUSTED_ORIGINS = ['https://*.argentum.ua','https://127.0.0.1']
 ALLOWED_HOSTS = ["*"]
 
 # Database PostgreSQL
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME":   os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE"),
+        'NAME': os.environ.get("SQL_DATABASE"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': os.environ.get("SQL_ENGINE"),
-            'NAME': os.environ.get("SQL_DATABASE"),
-            'USER': os.environ.get("SQL_USER"),
-            'PASSWORD': os.environ.get("SQL_PASSWORD"),
-            'HOST': os.environ.get("SQL_HOST"),
-            'PORT': os.environ.get("SQL_PORT"),
-        }
-    }
+}
 
 # https://stackoverflow.com/questions/69490806/wagtail-formbuilder-submissions-are-not-sending-emails
 if DEBUG:
