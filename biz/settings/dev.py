fab49f8e8238e4c7edeea49a690df477da8c9f5b
Andriy Voznyuk <andreyv@ukr.net>
2022-09-12 19:59:23 +0200

    dev-prod settings

--- biz/settings/dev.py
+++ biz/settings/dev.py
@@ -1,11 +1,12 @@
 from .base import *
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG")
+SECRET_KEY = os.environ.get("SECRET_KEY", "@WTREGVw4tgthg536UJET7iujETYJ4e67ujET7ietyj")
+DEBUG = os.environ.get("DEBUG", True)
+
 print('DEBUG=', DEBUG)
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 CSRF_TRUSTED_ORIGINS = ['https://*.argentum.ua','https://127.0.0.1']
 
@@ -14,16 +15,25 @@ CSRF_TRUSTED_ORIGINS = ['https://*.argentum.ua','https://127.0.0.1']
 ALLOWED_HOSTS = ["*"]
 
 # Database PostgreSQL
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get("SQL_ENGINE"),
-        'NAME': os.environ.get("SQL_DATABASE"),
-        'USER': os.environ.get("SQL_USER"),
-        'PASSWORD': os.environ.get("SQL_PASSWORD"),
-        'HOST': os.environ.get("SQL_HOST"),
-        'PORT': os.environ.get("SQL_PORT"),
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME":   os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.environ.get("SQL_ENGINE"),
+            'NAME': os.environ.get("SQL_DATABASE"),
+            'USER': os.environ.get("SQL_USER"),
+            'PASSWORD': os.environ.get("SQL_PASSWORD"),
+            'HOST': os.environ.get("SQL_HOST"),
+            'PORT': os.environ.get("SQL_PORT"),
+        }
+    }
+
 # https://stackoverflow.com/questions/69490806/wagtail-formbuilder-submissions-are-not-sending-emails
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
